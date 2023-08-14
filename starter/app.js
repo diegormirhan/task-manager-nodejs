2a3a92e017abb090f9405a79a1198ae6cb000452
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-08-14 18:05:05 -0300

    Created Env

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,10 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}`))
