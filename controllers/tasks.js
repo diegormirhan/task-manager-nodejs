6ee5c4d9a474f6a72a6c74ed756ea88bc549e82c
Diego R. Mirhan <mirhan.diego@gmail.com>
2023-08-19 19:36:50 -0300

    Schema configuration #007

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,16 @@
+const Task = require('../models/Task')
+
 const getAllTasks = (req, res) => {
     res.send('get all tasks')
 }
 
-const createTask = (req, res) => {
-    res.json(req.body)
+const createTask = async (req, res) => {
+    try {
+        const task = await Task.create(req.body)
+        res.status(201).json({ task })
+    } catch (error) {
+        res.status(500).json({ error })
+    }
 }
 
 const getTask = (req, res) => {
--- models/Task.js
+++ models/Task.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose');
+const TaskSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, 'Must provide name!'],
+        trim: true,
+        maxlength: [20, 'Name cannot be more than 20 characters!']
+    },
+    completed:{
+        
+    }
+})
+
+module.exports = mongoose.model('Task', TaskSchema);
